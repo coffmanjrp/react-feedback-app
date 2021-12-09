4f9c6ac1dac78a28338f53bbef10c343d40d8ba2
coffmanjrp <coffmanjrp@gmail.com>
2021-12-08 18:23:03 -0800

    Add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,15 @@ import { FC } from 'react';
 
 type Props = {
   text?: string;
+  bgColor?: string;
+  textColor?: string;
 };
 
-const Header: FC<Props> = ({ text }) => {
+const Header: FC<Props> = ({ text, bgColor, textColor }) => {
+  const headerStyles = { backgroundColor: bgColor, color: textColor };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -16,6 +20,8 @@ const Header: FC<Props> = ({ text }) => {
 
 Header.defaultProps = {
   text: 'Feadback UI',
+  bgColor: 'rgba(0, 0, 0, 0.4)',
+  textColor: '#ff6a95',
 };
 
 export default Header;
